f23a71734eb3e270a3ca6e04a3d9977d69f4fb8f
Udi Oron <udioron@gmail.com>
2017-02-07 17:05:22 +0200

    added some forms

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,5 +6,7 @@ app_name = "expenses"
 
 urlpatterns = [
     url(r'^$', views.list, name="list"),
+    url(r'^create/$', views.create, name="create"),
     url(r'^([0-9]+)/$', views.detail, name="detail"),
+    url(r'^feedback/$', views.send_feedback, name="feedback"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,18 +1,71 @@
+from django import forms
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from . import models
 
+
 def list(request):
     qs = models.Expense.objects.all()
-    total = sum(o.amount for o in qs)  # there is a better way to do this (aggregation)
+    total = sum(
+        o.amount for o in qs)  # there is a better way to do this (aggregation)
     return render(request, "expenses/expense_list.html", {
         'total': total,
         'objects': qs,
     })
 
+
 def detail(request, id):
     o = models.Expense.objects.get(id=id)
     return render(request, "expenses/expense_detail.html", {
         'object': o,
+    })
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = models.Expense
+        fields = "__all__"
+
+
+def create(request):
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            o = form.save()
+            return redirect(reverse("expenses:detail", args=(o.id,)))
+    else:
+        form = ExpenseForm()
+
+    return render(request, "expenses/expense_form.html", {
+        'form' : form,
+    })
+
+
+SUBJECTS = (
+    ("error", "Error in site"),
+    ("attention", "I need attention"),
+    ("other", "Other"),
+)
+
+
+class FeebackForm(forms.Form):
+    email = forms.EmailField()
+    subject = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea())
+    phone = forms.CharField(required=False)
+
+
+def send_feedback(request):
+    if request.method == "POST":
+        form = FeebackForm(request.POST)
+        if form.is_valid():
+            print("feeback sent", form.cleaned_data)
+            return redirect(reverse("expenses:list"))
+    else:
+        form = FeebackForm()
+
+    return render(request, "expenses/feedback_form.html", {
+        'form': form,
     })
