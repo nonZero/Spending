7f67e3dadd9e5e2d429a1c2624cdab08f2802711
Udi Oron <udioron@gmail.com>
2017-02-07 14:58:29 +0200

    added list view for expenses (and exercise)

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+def list(request):
+    qs = models.Expense.objects.all()  # !!!!! QuerySet
+    return render(request, "expenses/expense_list.html", {
+        'foo': 706,
+        'objects': qs,
+    })
+
+# TODO: 1. create a detail() view function, on /23/ that shows all expense info
+# TODO: 2. Link from list view <a href="....">click me</a> and back
+
