8f16bbf278014d2d85c727928e18a05abf27e486
Udi Oron <udioron@gmail.com>
2017-02-07 16:23:02 +0200

    cleanup

--- expenses/views.py
+++ expenses/views.py
@@ -5,14 +5,11 @@ from . import models
 
 def list(request):
     qs = models.Expense.objects.all()
-    total = sum(o.amount for o in qs)
+    total = sum(o.amount for o in qs)  # there is a better way to do this (aggregation)
     return render(request, "expenses/expense_list.html", {
         'total': total,
         'objects': qs,
     })
-
-# TODO: 1. create a detail() view function, on /23/ that shows all expense info
-# TODO: 2. Link from list view <a href="....">click me</a> and back
 
 def detail(request, id):
     o = models.Expense.objects.get(id=id)
