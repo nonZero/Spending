77aa1b3914fc0d616e4612657b0df5408f73a8b5
Udi Oron <udioron@gmail.com>
2017-02-07 11:52:56 +0200

    Added hello with kw params

--- spending/urls.py
+++ spending/urls.py
@@ -28,6 +28,7 @@ from django.http import HttpResponse
 def hello(request):
     return HttpResponse("hello!<b>hello!")
 
+
 # http://127.0.0.1:8000/boom/
 # protocol: http
 # host:port(default 80): 127.0.0.1:8000
@@ -41,15 +42,27 @@ def magic(request):
     html = "The magic number is: {}!!!".format(random.randint(1, 10))
     return HttpResponse(html)
 
-def hello_name(request, name):
-    return HttpResponse("hello <b>{}</b>!".format(name))
+
+def hello_name(request, name, lucky):
+    return HttpResponse(
+        "hello <b>{}</b> Your lucky number is {}!".format(name, lucky))
+
+
+def add(request, x, y):
+    x = int(x)
+    y = int(y)
+    return HttpResponse("{} + {} = {}".format(x, y, x + y))
+
 
 # /add/10/20/  ==>  10 + 20 = **30**
 
 
 urlpatterns = [
     url(r"^$", hello),
-    url(r"^hello/([a-z]+)/$", hello_name),
+    url(r"^add/(\d+)/(\d+)/$", add),
+    url(r"^hello/([a-z]+)/$", hello_name, kwargs={'lucky': 22}),
+    url(r"^hello/([a-z]+)/([0-9]+)/$", hello_name),
+    url(r"^hello/(?P<lucky>[0-9]+)/(?P<name>[a-z]+)/$", hello_name),
     url(r"^boom/$", boom),
     url(r"^magic/$", magic),
     url(r'^admin/', admin.site.urls),
