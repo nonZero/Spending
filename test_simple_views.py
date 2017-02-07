137531eac1b7363222893ef51c618ca7f4fb99b6
Udi Oron <udioron@gmail.com>
2017-02-07 12:19:52 +0200

    added json test

--- test_simple_views.py
+++ test_simple_views.py
@@ -6,8 +6,16 @@ class SimpleViewsTests(TestCase):
         url = "/hello/udi/"
         resp = self.client.get(url)
         self.assertContains(resp, "hello <b>udi</b> Your lucky number is 22!")
+        # self.assertEquals(resp.content, "sadsad")
 
     def test_bad_url(self):
         url = "/kuku/324234/"
         resp = self.client.get(url)
+
         self.assertEquals(resp.status_code, 404)
+
+    def test_add_json(self):
+        x, y = 52, 16
+        url = "/add/{}/{}/json/".format(x, y)
+        resp = self.client.get(url)
+        self.assertJSONEqual(resp.content, {"result": x + y})
